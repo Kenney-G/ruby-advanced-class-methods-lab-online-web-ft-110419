522e0f6358d4c96b8c21a3028dfc51a23dcff56d
Ken Goad <goadke@g.cofc.edu>
2019-11-18 20:08:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,49 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+ 
+ 
+ def Song.create
+   song = Song.new
+   song.save
+   song
+end
 
-  def self.all
+def self.new_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    song
+end
+
+def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    song
+end
+
+def self.all
     @@all
-  end
+end
+ 
+def Song.find_by_name(name)
+   Song.all.detect{|s| s.name == "#{name}"}
+end
+ 
+ def Song.find_or_create_by_name(name)
+   Song.find_by_name(name) || Song.create_by_name(name)
+ end
+ 
+ def Song.alphabetical
+   @name = name
+   binding.pry
+   @@all.each {|s| puts s.name.join(" ").sort}
+   binding.pry
+end
 
   def save
     self.class.all << self
   end
-
+ 
 end
